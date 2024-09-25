4dec55c7f8e9f2744b89bf965c9877faea3fd4c6
PeppePentrelli <peppepentrelli95@gmail.com>
2024-09-26 00:58:52 +0200

    Add files via upload

---
+++ Selfwork-variabili-1/Selfwork-01.js
@@ -0,0 +1,16 @@
+let num1 = 5
+let num2 = 7
+console.log(num1);
+console.log(num2);
+
+let stringa = 'ciao,a tutti!'
+console.log(stringa);
+
+stringa = 'benvenuti'
+console.log(stringa);
+
+const PIGRECO = 3.14159265359
+console.log(PIGRECO);
+
+PIGRECO = 20
+console.log(PIGRECO);
