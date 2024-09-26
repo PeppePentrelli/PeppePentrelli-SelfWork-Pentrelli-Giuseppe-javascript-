79b2db84f1dd6b566ed747a725d48b87209a9916
PeppePentrelli <peppepentrelli95@gmail.com>
2024-09-26 21:16:12 +0200

    Add files via upload

--- Selfwork-variabili-1/Selfwork-01.js
+++ Selfwork-variabili-1/Selfwork-01.js
@@ -1,19 +1,20 @@
 let num1 = 5
 let num2 = 7
-console.log(num1);
-console.log(num2);
+console.log('il valore della variabile num1 è: ' + num1);
+console.log(`il valore della variabile num2 è: ${num2} `);
 
 let stringa = 'ciao,a tutti!'
-console.log(stringa);
 
 stringa = 'benvenuti'
-console.log(stringa);
+console.log(`il valore della variabile stringa è ${stringa}`);
 
 const PIGRECO = 3.14159265359
-console.log(PIGRECO);
+console.log(`il valore di Pgreco è ${PIGRECO}`);
+
 
 // PIGRECO = 20
-// console.log(PIGRECO);
+console.log(PIGRECO);
+
 
 
 // esercizio operatori-1
@@ -34,4 +35,17 @@ console.log(media / 5);
 console.log('la somma tra i numeri equivale a ' + somma);
 
 console.log('la media tra i numeri equivale a ' + media);
+
+// SELFWORK OPERATORI-2
+
+let years = 2024
+let età = 1995
+let conut_age = years - età
+  
+console.log(conut_age)
+
+let cent = 100
+let forcent = cent - conut_age
+
+console.log(`hai ${conut_age} anni e te ne mancano ${forcent} per arrivare a 100`)
 
