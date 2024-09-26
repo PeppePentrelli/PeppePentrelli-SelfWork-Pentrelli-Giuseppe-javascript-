19397c2f59ec707946f00e3042b82191a9c4e542
PeppePentrelli <peppepentrelli95@gmail.com>
2024-09-26 08:48:29 +0200

    Add files via upload

--- Selfwork-variabili-1/Selfwork-01.js
+++ Selfwork-variabili-1/Selfwork-01.js
@@ -12,5 +12,26 @@ console.log(stringa);
 const PIGRECO = 3.14159265359
 console.log(PIGRECO);
 
-PIGRECO = 20
-console.log(PIGRECO);
+// PIGRECO = 20
+// console.log(PIGRECO);
+
+
+// esercizio operatori-1
+
+let a = 1;
+let b = 2;
+let c = 3;
+let d = 4;
+let e = 5;
+let somma = a + b + c + d + e
+console.log (somma)
+
+let media = somma / 5
+
+console.log(media / 5);
+
+
+console.log('la somma tra i numeri equivale a ' + somma);
+
+console.log('la media tra i numeri equivale a ' + media);
+
