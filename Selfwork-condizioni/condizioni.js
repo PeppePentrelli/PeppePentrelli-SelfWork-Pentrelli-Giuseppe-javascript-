5778bc21182a1684a81d7a8658bf8f1cd33b9522
PeppePentrelli <peppepentrelli95@gmail.com>
2024-09-29 15:24:56 +0200

    Add files via upload

--- Selfwork-condizioni/condizioni.js
+++ Selfwork-condizioni/condizioni.js
@@ -24,7 +24,7 @@ else {
 }
 
 
-// Selfwork-condizioni-1 / uso swich.
+// // Selfwork-condizioni-1 / uso swich.
 
 let voto = prompt('inserisci il tuo voto');
 switch (true) {
@@ -53,4 +53,57 @@ switch (true) {
         console.log('inserisci un voto compreso tra 0 e 30');
         break
 
+}
+
+
+// Selfwork-condizioni-2 / uso di if-,else.
+
+let temperatura = prompt('inserisci la temperatura')
+
+if (temperatura <= -10) { console.log('copriti...ancora ti raffreddi'); }
+
+else if (temperatura < 0 && temperatura > -10) {
+    console.log("non è tanto il freddo quanto l'umidita");
+}
+
+if (temperatura < 20 && temperatura >= 0) {
+    console.log('non ci sono piu le mezze stagioni');
+}
+else if (temperatura < 30 && temperatura > 20) {
+    console.log('mi dia una peroni sudata'); -222
+}
+else if (temperatura >= 30) {
+    console.log('lu mare, lu sole, lu ientu');
+}
+
+
+
+// Selfwork-condizioni-2 / uso di swich.
+
+let gradi = prompt('inserisci i gradi');
+
+switch (true) {
+    case gradi <= -10:
+        console.log('copriti...ancora ti raffreddi');
+        break
+
+    case gradi < 0 && gradi > -10:
+        console.log("non è tanto il freddo quanto l'umidita");
+        break
+
+    case gradi < 20 && gradi >= 0:
+        console.log("non ci sono piu le mezze stagioni");
+        break
+
+    case gradi < 30 && gradi > 20:
+        console.log('mi dia una personi sudata');
+        break
+
+    case gradi >= 30:
+        console.log('lu mare, lu sole, lu ientu');
+        break
+
+    default:
+        console.log('inserisci una temperatura corretta');
+        break
 }
