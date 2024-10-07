b373c9f82849a5bdac568afeac60d5e7e2e84a45
Pentrelli Giuseppe <peppepentrelli95@gmail.com>
2024-10-07 11:47:32 +0200

    aggiunto selfrowrk funzioni 1

--- Selfwork-funzioni/funzioni.js
+++ Selfwork-funzioni/funzioni.js
@@ -34,3 +34,47 @@ console.log(array_1[0][0],
     array_2[4][1],
     array_1[0][2]);
 
+
+
+// Selfwork funzioni 1
+
+// lancio di dadi giocatore 1
+
+let scelta_utente = Number(prompt('Quanti tiri effettuerai a questo turno?'));
+
+function lancio_dado (numero_lanci) {
+let somma = 0
+
+for (let i = 0; i < numero_lanci; i++) {
+    let tiro = Math.floor(Math.random() * (6 - 1) + 1) +1
+    somma += tiro;   
+
+    console.log('tiro', i+1 ,':', tiro );
+}
+return somma;
+
+}
+let risultato = lancio_dado(scelta_utente)
+console.log(`Giocatore 1 hai tirato il dato ${scelta_utente} volte e hai accumulato ${risultato} `);
+
+
+
+// lancio di dadi giocatore 2
+
+
+let scelta_utente2 = Number(prompt('Quanti tiri effettuerai a questo turno?'));
+
+function lancio_dado (numero_lanci) {
+let somma = 0
+
+for (let i = 0; i < numero_lanci; i++) {
+    let tiro = Math.floor(Math.random() * (6 - 1) + 1) +1
+    somma += tiro;   
+
+    console.log('tiro', i+1 ,':', tiro );
+}
+return somma;
+
+}
+let risultato2 = lancio_dado(scelta_utente2)
+console.log(`Giocatore 2 hai tirato il dato ${scelta_utente2} volte e hai accumulato ${risultato2} `);
