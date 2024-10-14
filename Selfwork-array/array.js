1131a4e237df1064e8b81ec7902df662f755790f
Pentrelli Giuseppe <peppepentrelli95@gmail.com>
2024-10-15 00:38:55 +0200

    Aggiunto primo Selfork sugli array

---
+++ Selfwork-array/array.js
@@ -0,0 +1,15 @@
+// SelfWork Array 1
+
+let numeri = [3,7,-2,5, 8, 1, 2, 5, 6, -4, 12, 25];
+
+let numeri_2 = [3,7,-2,5, 8, 1, 2, 5, 6, -4, 12, 25];
+
+
+let ordine_decrescente = numeri.sort(( a, b) => b - a) ;
+
+let ordine_crescente = numeri_2.sort(( a, b) => a - b ) ;
+
+
+
+console.log(ordine_decrescente);
+console.log(ordine_crescente);
