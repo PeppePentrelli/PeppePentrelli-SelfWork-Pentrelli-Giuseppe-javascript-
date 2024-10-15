ecdf1065f523dec2d9c18ffa5c240be7f1fbf7a4
Pentrelli Giuseppe <peppepentrelli95@gmail.com>
2024-10-15 21:32:47 +0200

    Aggiunto Selfwork Array 2

--- Selfwork-array/array.js
+++ Selfwork-array/array.js
@@ -1,15 +1,48 @@
+// SelfWork Array 2
+
+
+let numeri = [3, 5, 10, 2, 8];
+
+let somma = numeri.reduce((acc, n) => acc + n);
+
+let media = somma / numeri.length;
+
+let = numeri_minori = numeri.filter((numeri) => numeri < 7);
+
+let = numeri_maggiori = numeri.filter((numeri) => numeri > 7);
+
+let tot_valori_minori = numeri_minori.length;
+
+let tot_valori_maggiori = numeri_maggiori.length;
+
+
+
+console.log(`I numeri dell'array sono ${numeri}`);
+console.log(`La media equivale a ${media}`);
+console.log(`I numeri minori della media sono ${numeri_minori}`);
+console.log(`I numeri maggiori della media sono ${numeri_maggiori}`);
+console.log(`In totale i numeri minori della media sono ${tot_valori_minori} mentre i numeri maggiori della media sono ${tot_valori_maggiori}`);
+
+
+
+
+
+
+
+
+
 // SelfWork Array 1
 
-let numeri = [3,7,-2,5, 8, 1, 2, 5, 6, -4, 12, 25];
+// let numeri = [3,7,-2,5, 8, 1, 2, 5, 6, -4, 12, 25];
 
-let numeri_2 = [3,7,-2,5, 8, 1, 2, 5, 6, -4, 12, 25];
-
-
-let ordine_decrescente = numeri.sort(( a, b) => b - a) ;
-
-let ordine_crescente = numeri_2.sort(( a, b) => a - b ) ;
+// let numeri_2 = [3,7,-2,5, 8, 1, 2, 5, 6, -4, 12, 25];
 
 
+// let ordine_decrescente = numeri.sort(( a, b) => b - a) ;
 
-console.log(ordine_decrescente);
-console.log(ordine_crescente);
+// let ordine_crescente = numeri_2.sort(( a, b) => a - b ) ;
+
+
+
+// console.log(ordine_decrescente);
+// console.log(ordine_crescente);
