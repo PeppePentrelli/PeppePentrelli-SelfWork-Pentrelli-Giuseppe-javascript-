5a173968f574f61ce05dd6b2509d7192fcf26976
Pentrelli Giuseppe <peppepentrelli95@gmail.com>
2024-10-20 14:26:23 +0200

    aggiunto primo selfwork oggetti

---
+++ Selfwork-oggetti/oggetti.js
@@ -0,0 +1,15 @@
+// SELFWORK OGGETTI 1-2-3 IN FILA:
+
+
+/// SELFWORK OGGETTI 1:
+
+
+let persona = {
+
+    nome: 'Giuseppe',
+    cognome: 'Pentrelli',
+    età: 29
+
+};
+
+console.log(`Ciao a tutti il mio nome è ${persona.nome}`);
